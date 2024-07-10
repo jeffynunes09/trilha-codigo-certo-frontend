263f120c02217c879987e5c90e18e4b06c8996a6
Jefferson Nunes <nunesjefferson425@gmail.com>
2024-07-10 08:22:50 -0300

    changes :css do card de projetos

--- vite-project/src/components/cardProjetos.jsx
+++ vite-project/src/components/cardProjetos.jsx
@@ -5,7 +5,12 @@ function CardProjetos({name,description}) {
     <div className='card'>
         <h1 className='title-projeto'>{name}</h1>
         <p className='description-projeto'>{description}</p>
-
+        
+      <div className='.contaier-buttons'>
+      <button id='editar'>EDITAR</button>
+      <button id='excluir'>EXCLUIR</button>
+      </div>
+       
     </div>
   )
 }
